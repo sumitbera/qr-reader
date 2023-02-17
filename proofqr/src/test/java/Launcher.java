4090c0b1b2b0cdc15be275823ceccf604615170d
sumitbera <sumit.bera@live.in>
2023-02-17 12:11:50 +1100

    Added QR decoding logic and QR url locator value using img tag

--- proofqr/src/test/java/Launcher.java
+++ proofqr/src/test/java/Launcher.java
@@ -1,3 +1,6 @@
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -8,6 +11,8 @@ import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,12 +21,13 @@ import java.time.Duration;
 
 public class Launcher {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException, NotFoundException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://test-studio.oliu.id/login");
-        Thread.sleep(3000);
-        //Delete QR screenshot
+        Thread.sleep(2000);
+
+        //Delete QR screenshot if exists in screenshots folder
         try {
             Files.deleteIfExists(Paths.get("screenshots/qr.png"));
         } catch (IOException e) {
@@ -29,11 +35,15 @@ public class Launcher {
         }
         System.out.println("Screenshot deleted...");
 
-        //Capture screenshot of QR
+        //Wait for QR code to Load
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.MuiBox-root.css-194mb91")));
-        System.out.println("QR Code Found on web page");
-        WebElement qrCode = driver.findElement(By.cssSelector("div.MuiBox-root.css-194mb91"));
+        System.out.println("-------QR Code Found on web page-------");
+
+        //Find QR code URL
+        WebElement qrCode = driver.findElement(By.cssSelector("img[src*='data']"));
+
+        //Store QR code in screenshot directory
         File source = qrCode.getScreenshotAs(OutputType.FILE);
         File dest = new File(System.getProperty("user.dir") + "/screenshots/qr.png");
         try {
@@ -41,6 +51,15 @@ public class Launcher {
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+        //Decode QR code captured from the login screen
+        System.out.println("############################## QR DECODING STARTED #################################################");
+        BufferedImage bfImage = ImageIO.read(new File("screenshots/qr.png"));
+        LuminanceSource luminanceSource = new BufferedImageLuminanceSource(bfImage);
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
+        Result resultUrl = new MultiFormatReader().decode(binaryBitmap);
+        System.out.println("Invitation URL: " + resultUrl.getText());
+        System.out.println("############################## QR DECODING COMPLETED ###############################################");
         driver.quit();
     }
+
 }
